7bb1af9380d9ecd59237a3491175560283f5a926
juan25258 <juan25257@gmail.com>
2023-08-28 17:09:36 -0300

    quite la barra detras el node_modules/

--- src/controllers/itineraries.Controller.js
+++ src/controllers/itineraries.Controller.js
@@ -1,4 +1,4 @@
-const Itinerary = require('../models/Itinerary');
+const Itinerary = require("../models/Itinerary");
 
 module.exports = {
   getItineraries: async (req, res) => {
@@ -6,16 +6,31 @@ module.exports = {
       const itineraries = await Itinerary.find();
       res.json(itineraries);
     } catch (error) {
-      res.status(500).json({ error: 'Error fetching itineraries' });
+      res.status(500).json({ error: "Error fetching itineraries" });
+    }
+  },
+  getItineraryForCity: async (req, res) => {
+    const cityId = req.params.cityId;
+    try {
+      const itinerary = await Itinerary.findOne({ cityId });
+      if (!itinerary) {
+        return res.status(404).json({ error: "Itinerary not found" });
+      }
+      console.log('Itinerary response:', itinerary); 
+      res.json(itinerary);
+    } catch (error) {
+      console.error('Error fetching itinerary:', error); 
+      res.status(500).json({ error: "Error fetching itinerary" }); 
     }
   },
   
+
   addItinerary: async (req, res) => {
     try {
       const newItinerary = await Itinerary.create(req.body);
       res.status(201).json(newItinerary);
     } catch (error) {
-      res.status(500).json({ error: 'Error creating itinerary' });
+      res.status(500).json({ error: "Error creating itinerary" });
     }
   },
 
@@ -28,20 +43,19 @@ module.exports = {
       );
       res.json(updatedItinerary);
     } catch (error) {
-      res.status(500).json({ error: 'Error updating itinerary' });
+      res.status(500).json({ error: "Error updating itinerary" });
     }
   },
 
   deleteItinerary: async (req, res) => {
     try {
       let { id } = req.query;
-      await Itinerary.deleteOne({_id: id});
+      await Itinerary.deleteOne({ _id: id });
       res.status(201).json({
-        message: "Itinerary has been deleted"
+        message: "Itinerary has been deleted",
       });
-
     } catch (error) {
-      res.status(500).json({ error: 'Error deleting itinerary' });
+      res.status(500).json({ error: "Error deleting itinerary" });
     }
-  }
+  },
 };
